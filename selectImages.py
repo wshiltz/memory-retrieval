b66a7e07778cfac3d82f6b1974662fb6af0e7f11
wshiltz <wyattshiltz@outlook.com>
2025-04-01 16:06:27 -0400

    Gathered 800 photos
    Created a program to randomly select 800 photos from the PISC (People in Social Contexts) database to use for our model. The original database is not available here due to its large size.

---
+++ selectImages.py
@@ -0,0 +1,22 @@
+import random
+import os
+import shutil
+
+# Randomly selects x images from one directory to move to another directory
+def main():
+    count = 800
+    # I used ./piscDataset and ./examplePhotos/pisc respectivly
+    datasetDir = "INSERT INITIAL DATASET DIRECTORY HERE"
+    subsetDir = "INSERT DESTINATION DIRECTORY HERE"
+    
+    dataset = os.listdir(datasetDir)
+    subset = random.sample(dataset, count)
+
+    for image in subset:
+        shutil.copy(f"{datasetDir}/{image}", f"{subsetDir}/{image}")
+        print(f"Successfully copied {image}")
+
+    print(f"Successfully copied {count} images")
+
+if __name__ == "__main__":
+    main()
